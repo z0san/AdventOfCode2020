0ca039ce70e0657a80fa1503398f065de95f466e
Zohar Singer <injapanthree@gmail.com>
2020-12-03 00:49:34 -0500

    finnished day 1

--- day_1/main.hs
+++ day_1/main.hs
@@ -1,3 +1,36 @@
+-- for file reading
+import System.IO
+import Control.Monad
+
+
 main = do 
-    let fac n = if n == 0 then 1 else n * fac (n-1)
-    print (fac (5))
+    contents <- readFile "input.txt"
+    let test = map readInt . words $ contents
+    print test 
+    putStrLn "starting..."
+    putStr "Found: "
+    let result = testAll test
+    print result
+    let (fst, snd) = result
+    putStr "multiplied together are => " 
+    print (fst * snd)
+
+readInt :: String -> Int
+readInt = read
+
+testSingle :: Int -> [Int] -> (Int, Int)
+testSingle x (hd : tl) = 
+    if x + hd == 2020
+        then (x, hd)
+    else testSingle x tl
+
+testSingle x [] = (0, 0)
+
+
+testAll :: [Int] -> (Int, Int)
+testAll (x:xs) = 
+    let result = testSingle x xs
+    in case result of 
+        (0, 0) -> (testAll xs)
+        otherwise -> result
+testAll [] = (0, 0)
